1c021c64798866664273c7ee216fd97cd574888e
Alek <ehrgein@gmail.com>
2022-11-14 13:08:04 -0300

    1

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const saltRounds = 10;
 
 const db = mysql.createConnection({
-  host: "keepmovingclothing.vercel.app/api",
+  host: "kmcclothingapi",
   user: "root",
   password: "dbpassword",
   database: "ecommerce_db",
