0b8afc3eb09846c56eb3352a79d61391fcfdc8a8
Alek <ehrgein@gmail.com>
2022-11-14 00:59:55 -0300

    1

--- src/components/BrowseByCategory.jsx
+++ src/components/BrowseByCategory.jsx
@@ -1,41 +1,42 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 import shirts from "../assets/category/shirts.webp";
 import bottoms from "../assets/category/bottoms.webp";
 import sneakers from "../assets/category/sneakers.webp";
 import outerwear from "../assets/category/outerwear.webp";
 import accesories from "../assets/category/accesories.webp";
 import discounted from "../assets/category/discounted.webp";
-import {CategoriesContext} from '../App'
-import {Link} from 'react-router-dom'
-
+import { CategoriesContext } from "../App";
+import { Link } from "react-router-dom";
 
 function BrowseByCategory() {
-
-  const categoriescontext = useContext(CategoriesContext)
-
+  const categoriescontext = useContext(CategoriesContext);
 
   return (
-    <div className='flex flex-wrap flex-columns-2 justify-center gap-2 mx-2 my-4'>
-      { 
-        categoriescontext.map(item => 
-          <div key={item.id} pic={item.categories_img} className='flex flex-wrap my-4'>
-            <div className="mt-4 flex w-full">
+    <div className="flex flex-wrap flex-columns-2 justify-center gap-2 mx-2 my-4">
+      {categoriescontext?.map((item) => (
+        <div
+          key={item.id}
+          pic={item.categories_img}
+          className="flex flex-wrap my-4"
+        >
+          <div className="mt-4 flex w-full">
             <Link to={`/products/${item.categories}`}>
               <div>
-                 <img alt={`${item.categories}`} className='mobileL:w-[190px] mobileL:h-[190px] mobilem:w-[170px] mobilem:h-[170px]  mobilexs:w-[130px] mobilexs:h-[130px]  ml-1' src={item.categories_img}/> 
-                  <h1 className='text-center font-normal capitalize text-lg'>{item.categories}</h1>
+                <img
+                  alt={`${item.categories}`}
+                  className="mobileL:w-[190px] mobileL:h-[190px] mobilem:w-[170px] mobilem:h-[170px]  mobilexs:w-[130px] mobilexs:h-[130px]  ml-1"
+                  src={item.categories_img}
+                />
+                <h1 className="text-center font-normal capitalize text-lg">
+                  {item.categories}
+                </h1>
               </div>
             </Link>
-            </div>
           </div>
-        )
-      }
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-
-
-export default BrowseByCategory
-
-
+export default BrowseByCategory;
--- src/components/NewArrivalsHome.jsx
+++ src/components/NewArrivalsHome.jsx
@@ -1,72 +1,84 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import {Link} from 'react-router-dom'
-import {ProductsContext} from '../App'
-import {motion} from 'framer-motion'
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { ProductsContext } from "../App";
+import { motion } from "framer-motion";
 
 function NewArrivalsHome() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
 
-  const [width, setWidth] = useState(0)
-  const carousel = useRef()
+  const productscontext = useContext(ProductsContext);
 
-  const productscontext = useContext(ProductsContext)
+  useEffect(() => {
+    setWidth(4200 - 900);
+  }, []);
 
-  useEffect(()=> {
-
-    setWidth(4200 - 900)
-
-  }, [])
-
-  
-
-  
   return (
-    <div>      
-        <div className="flex justify-center text-center mt-11">
-            <p className='4k:text-4xl text-2xl px-4 py-2 capitalize' > New Arrivals </p>
-        </div> 
-        <motion.div ref={carousel} className="carouselallitems">
-          <motion.div drag="x"
-          dragConstraints={{right:0, left:-width}} className='inner-carousel ml-6'>
-            {productscontext.slice(-13).map(item => 
-            <motion.div className='flex flex-col uppercase justify-center 4k:w-[400px] 4k:h-[400px] desktop:w-[370px] desktop:h-[380px]  laptopL:w-[330px] laptopL:h-[330px] laptop:w-[310px] laptop:h-[310px] md:w-[300px] md:h-[300px]
-            4k:my-20 4k:mx-12 desktop:mx-18 desktop:mt-12 text-sm laptopL:mx-4 laptop:mx-3 tablet:mx-2 laptop:mt-8 md:mt-8 mb-4 items-center' 
-            key={item.id} categories={item.categories} itemname={item.name} itemprice={item.price} itemimg={item.prod_img} >
-          
-              <motion.div className='4k:w-[350px] 4k:h-[350px] desktop:w-[300px] desktop:h-[300px] laptopL:w-[250px] laptopL:h-[250px] laptop:w-[220px] laptop:h-[220px] md:w-[230px] md:h-[230px]'>
+    <div>
+      <div className="flex justify-center text-center mt-11">
+        <p className="4k:text-4xl text-2xl px-4 py-2 capitalize">
+          {" "}
+          New Arrivals{" "}
+        </p>
+      </div>
+      <motion.div ref={carousel} className="carouselallitems">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel ml-6"
+        >
+          {productscontext?.slice(-13).map((item) => (
+            <motion.div
+              className="flex flex-col uppercase justify-center 4k:w-[400px] 4k:h-[400px] desktop:w-[370px] desktop:h-[380px]  laptopL:w-[330px] laptopL:h-[330px] laptop:w-[310px] laptop:h-[310px] md:w-[300px] md:h-[300px]
+            4k:my-20 4k:mx-12 desktop:mx-18 desktop:mt-12 text-sm laptopL:mx-4 laptop:mx-3 tablet:mx-2 laptop:mt-8 md:mt-8 mb-4 items-center"
+              key={item.id}
+              categories={item.categories}
+              itemname={item.name}
+              itemprice={item.price}
+              itemimg={item.prod_img}
+            >
+              <motion.div className="4k:w-[350px] 4k:h-[350px] desktop:w-[300px] desktop:h-[300px] laptopL:w-[250px] laptopL:h-[250px] laptop:w-[220px] laptop:h-[220px] md:w-[230px] md:h-[230px]">
                 <Link to={`/products/${item.categories}/${item.id}`}>
-                  <img alt={`${item.name} ${item.brand} $${item.price}`}
-                    className='grabimg'  onMouseDown={e => e.preventDefault()} src={item.prod_img}/>
-                </Link> 
-              </motion.div>
-              <div className='4k:mt-4 desktop:mt-4 laptopL:mt-4 tablet:mt-2 '>
-                <p className='font-bold text-center 4k:text-lg desktop:text-base'> {item.name}</p>
-                <Link>
-                  <p className='text-center 4k:text-lg text-sm mt-2'> {item.brand} </p>
+                  <img
+                    alt={`${item.name} ${item.brand} $${item.price}`}
+                    className="grabimg"
+                    onMouseDown={(e) => e.preventDefault()}
+                    src={item.prod_img}
+                  />
                 </Link>
-                <p className='text-base mt-1 font-semibold text-center 4k:text-lg desktop:text-base'> $ {item.price}  </p>      
+              </motion.div>
+              <div className="4k:mt-4 desktop:mt-4 laptopL:mt-4 tablet:mt-2 ">
+                <p className="font-bold text-center 4k:text-lg desktop:text-base">
+                  {" "}
+                  {item.name}
+                </p>
+                <Link>
+                  <p className="text-center 4k:text-lg text-sm mt-2">
+                    {" "}
+                    {item.brand}{" "}
+                  </p>
+                </Link>
+                <p className="text-base mt-1 font-semibold text-center 4k:text-lg desktop:text-base">
+                  {" "}
+                  $ {item.price}{" "}
+                </p>
               </div>
             </motion.div>
-            )}
-          </motion.div>
+          ))}
         </motion.div>
-        <div className='flex justify-center my-12'>
-            <Link className='text-white font-bold text-xl px-7 py-2 bg-black' to={`/newarrivals`} >
-              VIEW ALL
-            </Link>
-        </div>
+      </motion.div>
+      <div className="flex justify-center my-12">
+        <Link
+          className="text-white font-bold text-xl px-7 py-2 bg-black"
+          to={`/newarrivals`}
+        >
+          VIEW ALL
+        </Link>
+      </div>
 
-        <div>
-          
-        </div>
-
-
-
+      <div></div>
     </div>
-  )
+  );
 }
 
-export default NewArrivalsHome
-
-
-
-
+export default NewArrivalsHome;
