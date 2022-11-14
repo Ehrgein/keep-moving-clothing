bb018391195e342a391a88184f3a3b39000cf78c
Alek <ehrgein@gmail.com>
2022-11-14 15:00:30 -0300

    1

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://keepmovingclothing.vercel.app/",
+    origin: "https://keepmovingclothing.vercel.app",
     methods: ["GET", "POST"],
   })
 );
