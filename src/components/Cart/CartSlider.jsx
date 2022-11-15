b97e1137194a7cc36822cd90a13daa7aaaac6c47
Alek <ehrgein@gmail.com>
2022-11-15 15:56:05 -0300

    added link to cart images

--- src/components/Cart/CartSlider.jsx
+++ src/components/Cart/CartSlider.jsx
@@ -88,11 +88,13 @@ function CartSlider() {
                 className="scrollcart cart-anim flex justify-start cartborder my-2"
               >
                 <div className=" my-4 mobilem:h-32 mobilem:w-32 mobileL:w-[120px] mobileL:h-[120px]  mobilexs:w-[120px] mobilexs:h-[120px]">
-                  <img
-                    alt={`${item.name} ${item.brand} $${item.price}`}
-                    src={item.img}
-                    className="w-[120px] h-[120px]"
-                  />
+                  <Link to={`/products/${item.category}/${item.id}`}>
+                    <img
+                      alt={`${item.name} ${item.brand} $${item.price}`}
+                      src={item.img}
+                      className="w-[120px] h-[120px]"
+                    />
+                  </Link>
                 </div>
                 <div
                   className="uppercase font-medium ml-2 my-6 tracking-wide
