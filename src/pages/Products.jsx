00334f7d190dc4c74f68a5fbfb5529b5b0c4ae70
Alek <ehrgein@gmail.com>
2022-11-15 16:59:56 -0300

    added lazyloading to categories

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,21 +1,23 @@
-import React, {Suspense} from 'react'
-import Header from '../components/Header'
-import NewsLetter from '../components/NewsLetter'
-import AllProdByCat from '../components/Products/AllProdByCat'
-import Loading from './Loading'
+import React, { Suspense } from "react";
+import Header from "../components/Header";
+import NewsLetter from "../components/NewsLetter";
+import AllProdByCat from "../components/Products/AllProdByCat";
+import Loading from "./Loading";
 
-
+const LazyAllProdByCat = React.lazy(() =>
+  import("../components/Products/AllProdByCat")
+);
 
 function Products() {
   return (
-    <div className=''>
-        <Header/>
-        <Suspense fallback={<Loading/>}>
-          <AllProdByCat/>
-        </Suspense>
-        <NewsLetter/>
+    <div className="">
+      <Header />
+      <Suspense fallback={<Loading />}>
+        <LazyAllProdByCat />
+      </Suspense>
+      <NewsLetter />
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
