39c2702b1e67cf2a570eef5b1064a6164738e109
Alek <ehrgein@gmail.com>
2024-09-24 21:40:47 -0300

    Removed orders redirect

--- src/components/CheckoutModal.jsx
+++ src/components/CheckoutModal.jsx
@@ -9,7 +9,8 @@ function CheckoutModal({ purchasemodal, setonPurchasemodal }) {
       <div className="h-screen flex justify-center  items-center text-black text-2xl ">
         <motion.div className="flex flex-col cartshadow relative rounded-md justify-center bg-white md:py-12 py-8  md:px-24 px-4 cart-anim">
           <div className="absolute top-0 right-0 mt-2 mr-3">
-            <button aria-label="Close"
+            <button
+              aria-label="Close"
               className="4k:text-2xl desktop:text-xl laptop:text-xl  mobileL:text-xl font-medium"
               onClick={() => setonPurchasemodal(false)}
             >
@@ -25,7 +26,7 @@ function CheckoutModal({ purchasemodal, setonPurchasemodal }) {
               </h1>
               <FcCheckmark className="h-8 4k:h-14 w-8 ml-2 mobilexs:hidden tablet:block" />
             </div>
-            <h1 className="mt-3 4k:text-2xl desktop:text-xl laptopL:text-lg tablet:text-xl mobilexs:text-lg font-medium">
+            {/* <h1 className="mt-3 4k:text-2xl desktop:text-xl laptopL:text-lg tablet:text-xl mobilexs:text-lg font-medium">
               You can find your purchase details
               <Link
                 className="underline mobilexs:text-lg ml-2 4k:text-2xl desktop:text-xl"
@@ -33,7 +34,7 @@ function CheckoutModal({ purchasemodal, setonPurchasemodal }) {
               >
                 here
               </Link>
-            </h1>
+            </h1> */}
           </div>
         </motion.div>
       </div>
